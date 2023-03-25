ca8b2966db6ad1b96ad91b00c727be83a7541226
Navin Abraham <navin@Navins-MacBook-Air.local>
2023-03-24 23:26:23 -0600

    final commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 
-import React, {useState} from "react";
-import axios from 'axios'
+import React, {useState} from "react";  // Importing the React library and the useState hook
+import axios from 'axios' // Importing the axios library for making API requests
 
 function App() {
-  const[data,setData]= useState({})
-  const [location, setLocation] = useState('')
+  const[data,setData]= useState({}) // Initializing state for the weather data
+  const [location, setLocation] = useState('')  // Initializing state for the location search input
 
+  // Building the API URL with the user's search query
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=8b3c5b5fd2141cc22c662dc697897c2f`;
 
   
-  //connecting to the api
+  // Function to handle the location search when the user presses the Enter key
   const searchLocation = (event)=>{
     if(event.key==='Enter'){
       axios.get(url).then((response) =>{
-        setData(response.data)
-        console.log(response.data)
+        setData(response.data)  // Updating the weather data state with the API response data
       })
-      setLocation('')
+      setLocation('') // Resetting the location search input
     }
   
   }
@@ -35,33 +35,33 @@ function App() {
       <div className="container">
         <div className="top">
           <div className="location">
-            <p>{data.name}</p>
+            <p>{data.name}</p>  {/*Displaying the location name from the weather data state*/}
           </div>
           <div className="temp">
-            {data.main ? <h1> {data.main.temp.toFixed()} °C</h1> : null}
+            {data.main ? <h1> {data.main.temp.toFixed()} °C</h1> : null} {/* Displaying the temperature from the weather data state, if available */}
             
           </div>
           <div className="description">
-            {data.weather ? <p> {data.weather[0].main}</p> : null}
+            {data.weather ? <p> {data.weather[0].main}</p> : null}  {/* Displaying the weather description from the weather data state, if available */}
             
           </div>
         </div>
 
-
+        {/*Hides the bottom part of the page till a value is entered into the city name field */}
         {data.name !== undefined &&
 
             <div className="bottom">
             <div className="feels">
-              {data.main ? <p className="bold"> {data.main.feels_like.toFixed()} °C</p> : null}
+              {data.main ? <p className="bold"> {data.main.feels_like.toFixed()} °C</p> : null}  {/* Displaying the "feels like" temperature from the weather data state, if available*/}
               
               <p>Feels Like</p>
             </div>
             <div className="humidity">
-              {data.main ? <p className="bold"> {data.main.humidity} %</p> : null}
+              {data.main ? <p className="bold"> {data.main.humidity} %</p> : null}  {/* Displaying the humidity from the weather data state, if available*/}
               <p>Humidity</p>
             </div>
             <div className="wind">
-              {data.wind ? <p className="bold"> {data.wind.speed} MPH</p> : null}
+              {data.wind ? <p className="bold"> {data.wind.speed} MPH</p> : null} {/* Displaying the wind speed from the weather data state, if available */}
 
               <p >Wind Speed</p>
             </div>
@@ -74,3 +74,5 @@ function App() {
 }
 
 export default App;
+
+
